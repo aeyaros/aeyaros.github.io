08b115ee1f1c32cb27e1b7f6d6c8f38199a47069
Andrew Yaros <aeyaros@gmail.com>
2020-12-06 21:47:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -88,8 +88,7 @@ function start() {
 			+ ".moduleImage { height: 25vh; } "
 			+ ".buttonContainer { flex-flow: column nowrap; align-items: stretch; } "
 			+ "} "
-			+ ".linkButton:hover, .linkButton:active, .footerLink:hover, .footerLink:active { color: unset; background-color: unset; box-shadow: unset; } "
-			+ ".linkButton:hover .thearrow, .linkButton:active .thearrow { fill: unset; stroke: unset; } "
+			+ ".linkButton:hover, .linkButton:active, .footerLink:hover, .footerLink:active, .linkButton:hover .thearrow, .linkButton:active .thearrow { all: unset; } "
 		;
 		document.head.appendChild(mobileStyle);
 	}
