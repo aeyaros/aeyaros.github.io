b00dc27336f5217d8a7b487603209273f563767e
Andrew Yaros <aeyaros@gmail.com>
2020-12-07 05:12:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ function isMobile() {
 	return 'ontouchstart' in document.documentElement;
 }
 
-/* detect and adjust for mobile devices by testing whether there is a touchscreen */
-	if(isMobile()) {
-		console.log("Detected mobile touch event. Adjusting styles.")
-		let mobileStyle = document.createElement("style");
-		mobileStyle.innerText = `/* body { background-color: green; } */ .fixedBackground { background-attachment: scroll; } body { font-size: 14pt; } @media only screen and (orientation: portrait) { .moduleImage { height: 25vh; } .buttonContainer { flex-flow: column nowrap; align-items: stretch; } } .linkButton:hover, .linkButton:active { color: var(--buttonText); background-color: var(--backgroundColor); box-shadow: 0 5px 20px var(--buttonShadow); } .footerLink:hover, .footerLink:active { color: var(--buttonText); background-color: var(--darkerColor); box-shadow: box-shadow: 0 2px 10px var(--buttonShadow); } .linkButton:hover .thearrow, .linkButton:active .thearrow { fill: var(--buttonText); stroke: var(--buttonText); }`;
-		document.head.appendChild(mobileStyle);
-	}
-
 //disable right clicks
 document.addEventListener('contextmenu', event => event.preventDefault());
 
@@ -54,6 +46,14 @@ function changeSlide(path) {
 //set the initial slide
 function setInitialSlide() {
 	changeSlide(slidesPath + slideFiles[startingSlide]);
+	
+	/* detect and adjust for mobile devices by testing whether there is a touchscreen */
+	if(isMobile()) {
+		console.log("Detected mobile touch event. Adjusting styles.")
+		let mobileStyle = document.createElement("style");
+		mobileStyle.innerText = `/* body { background-color: green; } */ .fixedBackground { background-attachment: scroll; } body { font-size: 14pt; } @media only screen and (orientation: portrait) { .moduleImage { height: 25vh; } .buttonContainer { flex-flow: column nowrap; align-items: stretch; } } .linkButton:hover, .linkButton:active { color: var(--buttonText); background-color: var(--backgroundColor); box-shadow: 0 5px 20px var(--buttonShadow); } .footerLink:hover, .footerLink:active { color: var(--buttonText); background-color: var(--darkerColor); box-shadow: box-shadow: 0 2px 10px var(--buttonShadow); } .linkButton:hover .thearrow, .linkButton:active .thearrow { fill: var(--buttonText); stroke: var(--buttonText); }`;
+		document.head.appendChild(mobileStyle);
+	}
 }
 
 //increment to the next slide
