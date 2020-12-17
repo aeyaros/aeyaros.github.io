f193c3204e0b3ff084d2fc796b0d5fab45981cbe
Andrew Yaros <aeyaros@gmail.com>
2020-12-16 23:24:02 -0500

    Slide transition effect now works on firefox
    -titlebox has two absolute positioned divs inside:
    ---one to hold another background image: the opacity of this inner div is now the property being changed to create the transition effect
    ---another to hold the title text and description, with a much higher z-index

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ const slideInterval = 3000;
 /* When the page is loaded: set the background image based on the current time */
 const startingSlide = (Math.floor((new Date).getTime())) % n;
 
+//counter variable starts at the second image; we already displayed the first
+var slideCounter = startingSlide + 1;
+
+//inner title div starts out hidden
+var innerTitleDivHidden = true;
+
 //preload images
 var images = [];
 /* Start preloading at the starting slide
@@ -36,11 +42,17 @@ for(let i = startingSlide; i < startingSlide + n; i++) {
 	images.push(img)
 }
 
-//counter variable starts at the second image; we already displayed the first
-var slideCounter = startingSlide + 1;
+//change the opacity of the inner div when transitioning to a slide.
+function setInnerTitleDivOpacity(value) {
+	let corrected = Number(value);
+	if(corrected < 0) corrected = 0;
+	else if(corrected > 1) corrected = 1;
+	document.getElementById("titleBoxInner").style.opacity = String(corrected);
+}
 
-function changeSlide(path) {
-	document.getElementById("titleBox").style.backgroundImage = "url('" + path + "')";
+//change the background image path of an element
+function changeSlide(elementID, path) {
+	document.getElementById(elementID).style.backgroundImage = "url('" + path + "')";
 }
 
 //set the initial slide
@@ -52,13 +64,34 @@ function setInitialSlide() {
 		mobileStyle.innerText = `/* body { background-color: green; } */ .fixedBackground { background-attachment: scroll; } body { font-size: 14pt; } @media only screen and (orientation: portrait) { .moduleImage { height: 25vh; } .buttonContainer { flex-flow: column nowrap; align-items: stretch; } } .linkButton:hover, .linkButton:active { color: var(--buttonText); background-color: var(--backgroundColor); box-shadow: 0 5px 20px var(--buttonShadow); } .footerLink:hover, .footerLink:active { color: var(--buttonText); background-color: var(--darkerColor); box-shadow: 0 2px 10px var(--buttonShadow); } .linkButton:hover .thearrow, .linkButton:active .thearrow { fill: var(--buttonText); stroke: var(--buttonText); }`;
 		document.head.appendChild(mobileStyle);
 	}
-	
-	changeSlide(slidesPath + slideFiles[startingSlide]);
+
+	//change background of the inner slide to the starting slide
+	changeSlide("titleBox",slidesPath + slideFiles[startingSlide]);
 }
 
 //increment to the next slide
 function incrementSlide() {
-	changeSlide(slidesPath + slideFiles[slideCounter % n]);
+	//if the INNER div is HIDDEN
+		//change the INNER div background (so it is set before we transition to it)
+		//set opacity of inner div to 1
+
+	//if the INNER div is VISIBLE
+		//change the OUTER div background (so it is set before we transition to it)
+		//set opacity of the inner div to 0
+
+	//path of the image we are about to transition to
+	let pathToUse = slidesPath + slideFiles[slideCounter % n];
+
+	if(innerTitleDivHidden) {
+		changeSlide("titleBoxInner",pathToUse);
+		setInnerTitleDivOpacity(1.0)
+		innerTitleDivHidden = false;
+	} else {
+		changeSlide("titleBox", pathToUse);
+		setInnerTitleDivOpacity(0);
+		innerTitleDivHidden = true;
+	}
+
 	slideCounter += 1;
 }
 
