3215222a405516749ad412c4aff63c7d9c22c832
Andrew Yaros <aeyaros@gmail.com>
2023-03-23 22:58:35 -0400

    changes to fonts and colors
    background lighter than footer
    changes to fonts for buttons and module titles, as well as changes to font spacing
    buttons are colored and cycle through hues while scrolling

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function setInitialSlide() {
 	if (isMobile()) {
 		console.log("Detected mobile touch event. Adjusting styles.")
 		let mobileStyle = document.createElement("style");
-		mobileStyle.innerText = `:root { --heightTransition: 1s; } /* body { background-color: green; } */ .fixedBackground, body { background-attachment: scroll; } body { font-size: 14pt; } @media only screen and (orientation: portrait) { .moduleImage { height: 25vh; } .buttonContainer { flex-flow: column nowrap; align-items: stretch; } } .linkButton:hover, .linkButton:active { color: var(--buttonText); background-color: var(--backgroundColor); box-shadow: 0 5px 20px var(--buttonShadow); } .footerLink:hover, .footerLink:active { color: var(--buttonText); background-color: var(--darkerColor); box-shadow: 0 2px 10px var(--buttonShadow); } .linkButton:hover .thearrow, .linkButton:active .thearrow { fill: var(--buttonText); stroke: var(--buttonText); }`;
+		mobileStyle.innerText = `:root { --heightTransition: 1s; } .fixedBackground, body { background-attachment: scroll; } body { font-size: 14pt; } @media only screen and (orientation: portrait) { .moduleImage { height: 25vh; } .buttonContainer { flex-flow: column nowrap; align-items: stretch; } } .linkButton:hover, .linkButton:active { color: var(--buttonText); background-color: var(--backgroundColor); box-shadow: 0 5px 20px var(--buttonShadow); } .footerLink:hover, .footerLink:active { color: var(--buttonText); background-color: var(--darkerColor); box-shadow: 0 2px 10px var(--buttonShadow); } .linkButton:hover .thearrow, .linkButton:active .thearrow { fill: var(--buttonText); stroke: var(--buttonText); }`;
 		document.head.appendChild(mobileStyle);
 	}
 
@@ -145,3 +145,13 @@ function sendEmail() {
 	//go to mailto link
 	window.location = "mailto:" + shiftWord(shifted[0]) + "@" + shiftWord(shifted[1]) + "." + shiftWord(shifted[2]);
 }
+
+//change button color as scroll position changes
+//use event listener to detect change in scroll position
+//change css variable to a color with hue determined by (scrollpos * factor ) % 360
+document.addEventListener("scroll", function () {
+	let hueScaleFactor = 10;
+	let hue = (window.scrollY / hueScaleFactor) % 360;
+	let hsl = 'hsl(' + hue + 'deg 100% 25%)';
+	document.documentElement.style.setProperty('--buttonColor', hsl);
+});
